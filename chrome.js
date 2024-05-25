e14e446ad8764a41e7d69389c7132c8b1f9cbec7
Samia Cabral <devsamiac@gmail.com>
2024-05-25 20:22:22 -0300

    .

--- chrome.js
+++ chrome.js
@@ -1,6 +1,6 @@
 chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        files: ['content.js']
+        files: ['mudar_fonte.js', 'remover_img.js']
     });
 });
